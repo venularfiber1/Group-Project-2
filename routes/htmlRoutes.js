7c0441b77ec61272f56f35b29d4436dbe5554776
chefgonzo <chefgonzo@gmail.com>
2018-09-04 12:06:53 -0400

    npm passport update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,34 @@
 var db = require("../models");
 
+// Requiring path to so we can use relative routes to our HTML files
+// var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 module.exports = function (app) {
   // Load index page
   app.get("/", function (req, res) {
+    if (req.user) {
+      res.redirect("/budget");
+    }
     res.render("index", {});
   });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/budget");
+    }
+    res.render("index", {});
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/budget", isAuthenticated, function(req, res) {
+    res.render("budget", {});
+  });
+
   app.get("/budget", function (req, res) {
     res.render("budget", {});
   });
