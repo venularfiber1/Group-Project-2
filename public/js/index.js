7a7f4945c2e5d6c7e1f00c62c0fb45c4052c5be8
chefgonzo <chefgonzo@gmail.com>
2018-09-05 10:33:30 -0400

    chart.js integrated -- need to point our data at the charts

--- public/js/index.js
+++ public/js/index.js
@@ -103,3 +103,101 @@ expenseSubmitBtn.on("click", handleExpenseSubmit);
 incomeSubmitBtn.on("click", handleIncomeSubmit);
 deleteExpenseBtn.on("click", handleExpenseDelete);
 deleteIncomeBtn.on("click", handleIncomeDelete);
+
+
+
+
+// CHART FOR THE NET DIFFERENCE
+var ctx = $("#myChartNet");
+var myDoughnutChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Income", "Expense"],
+    datasets: [{
+      label: "# of Votes",
+      data: [19, 12],
+      backgroundColor: [
+        "rgba(75, 192, 192, 1)",
+        "rgba(255,99,132,1)"],
+    }]
+  },
+  options: { }
+});
+
+// CHART FOR INCOME
+var ctx = $("#myChartIncome");
+var myIncomeChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [{
+      label: "",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)"
+      ],
+      borderColor: [
+        "rgba(255,99,132,1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)"
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
+
+// CHART FOR EXPENSES
+var ctx = $("#myChartExpense");
+var myExpenseChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [{
+      label: "",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)"
+      ],
+      borderColor: [
+        "rgba(255,99,132,1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)"
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
